dd21835eca93dfadc6bb51e2ceb158d83be3a45f
Trung3Tech <trung3tech@gmail.com>
2021-06-09 18:38:06 +0700

    - sorting

--- CoffeeMintAdmin2/src/main/java/com/green/java/controller/CoffeeMintAdminController.java
+++ CoffeeMintAdmin2/src/main/java/com/green/java/controller/CoffeeMintAdminController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,20 +27,44 @@ public class CoffeeMintAdminController {
 	
 	@GetMapping("/")
 	public String showIndex(Model model) {
-		return showIndex(model, 1);
+		return showIndex(model, 1, null, null);
 	}
 	
 	@GetMapping("/page/{pageNum}")
-	public String showIndex(Model model, @PathVariable(name = "pageNum") int pageNum) {
+	public String showIndex(Model model, @PathVariable(name = "pageNum") int pageNum, @Param("sortBy") String sortBy, 
+			 @Param("sortDirection") String sortDirection) {
 		
-		Page<Order> page = orderService.getAllOrders(pageNum);
+		System.out.println("sortBy: " + sortBy  + " ==  " + "sortDirection");
 		
+		String direction = "asc";
+		
+		if (sortDirection != null && sortDirection.equals("asc")) {
+			direction = "desc";
+		}
+		
+		if (sortBy == null) {
+			sortBy = "name";
+		}
+		
+		Page<Order> page = orderService.getAllOrders(pageNum, sortBy, direction);
 		List<Order> orderList = page.getContent();
+		
+		long startCount = (pageNum -1) * OrderService.PAGE_SIZE + 1;
+		long endCount = startCount + OrderService.PAGE_SIZE - 1;
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
 		
 		model.addAttribute("orderList", orderList);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("totalItems", page.getTotalElements());
-		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("currentPage", pageNum); 
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		
+		//Sort
+		model.addAttribute("direction", direction);
+		model.addAttribute("sortBy", sortBy);
 		
 		return "data";
 	}
--- CoffeeMintAdmin2/src/main/java/com/green/java/service/OrderService.java
+++ CoffeeMintAdmin2/src/main/java/com/green/java/service/OrderService.java
@@ -1,11 +1,10 @@
 package com.green.java.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,8 +22,32 @@ public class OrderService {
 
 	public Page<Order> getAllOrders(int pageNum) {
 		
-		Pageable pageable = PageRequest.of(pageNum - 1, OrderService.PAGE_SIZE);
+		Pageable pageable;
 		
+		if (pageNum >= 1) {
+			pageable = PageRequest.of(pageNum - 1, OrderService.PAGE_SIZE);
+		} else {
+			pageable = PageRequest.of(0, OrderService.PAGE_SIZE);
+		}
+		return ordersRepository.findAll(pageable);
+	}
+	
+	public Page<Order> getAllOrders(int pageNum, String sortBy, String sortDirection) {
+		Sort sort = Sort.by(sortBy);
+		
+		if (sortDirection.equals("asc")) {
+			sort = sort.ascending();
+		} else {
+			sort = sort.descending();
+		}
+		
+		Pageable pageable;
+		
+		if (pageNum >= 1) {
+			pageable = PageRequest.of(pageNum - 1, OrderService.PAGE_SIZE, sort);
+		} else {
+			pageable = PageRequest.of(0, OrderService.PAGE_SIZE, sort);
+		}
 		return ordersRepository.findAll(pageable);
 	}
 
